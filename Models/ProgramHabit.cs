1090fdbc7592d6494317cd075e2877cff88c2572
idesignerse <heberandrade@gmail.com>
2021-04-22 15:17:38 +0200

    Creating Context

---
+++ Models/ProgramHabit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionsInOut.Models
+{
+    public class ProgramHabit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+    }
+}
